f8bc29e6d9f9d2af788d4464989e7ec0456c6009
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-01 19:31:06 +0300

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va afisa suma cifrelor unui numar n, n fiind citit de la tastatura */
+
+            int n;
+            
+            Console.WriteLine("Introduceti numarul:");
+            n = int.Parse(Console.ReadLine());
+
+            int sumaNumerelor = 0;
+
+             while (n != 0)
+            {
+                int ultimaCifra = n % 10;
+                sumaNumerelor = sumaNumerelor + ultimaCifra;
+
+                n = n / 10;
+            }
+
+            Console.WriteLine("\n"+"Suma numerelor este "+ sumaNumerelor + ".");
+        }
+    }
+}
